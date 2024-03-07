13a9453d82fc85ad74215ad02577833844b8499b
mariesoleilrioux <94701302+mariesoleilrioux@users.noreply.github.com>
2024-03-07 14:29:16 -0500

    Production interface b (#1)
    Co-authored-by: bear1922 <roxane.beauregard2@usherbrooke.ca>

---
+++ src/scriptA.js
@@ -0,0 +1,7 @@
+import './stylesA.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Le chargement du DOM est terminé');
+});
+  
--- src/scriptB.js
+++ src/scriptB.js
@@ -1,4 +1,4 @@
-import './styles.css';
+import './stylesB.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 document.addEventListener('DOMContentLoaded', function() {
